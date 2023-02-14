38fbe3e07c4bfee8fc35d4445a6bf693e5907af0
Adam Bogocz <adi_bog@o2.pl>
2023-02-14 17:36:02 +0100

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -23,28 +23,12 @@
         render();
     }
 
-const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
-    render();
-}
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li
-                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-                >
-                    <button class="js-done">zrobione?</button>    
-                <button class="js-remove">usuń</button>
-                    ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButtons, index) => {
@@ -60,6 +44,26 @@ const toggleTaskDone = (taskIndex) => {
                 toggleTaskDone(index);
             });
         });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
+                    <button class="js-done">zrobione?</button>    
+                    <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
 
